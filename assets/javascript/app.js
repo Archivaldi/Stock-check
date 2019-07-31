59f5debb897e7a74e3c795587b578c19ca332407
Artur Markov <artur.markov1860@gmail.com>
2019-07-31 11:34:51 -0700

    proper API Key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,28 @@
-function checkPrice(){
+stocks = [];
+
+$("#pushStock").on("click", function() {
     var symbol = $("input[type=text]").val();
-    var u = "https://sandbox.iexapis.com/stable/stock/"+symbol+"/quote?token=Tpk_ac0b33181d2e433d982b32f36d99a02c";
-    $.ajax({
-        method: "GET",
-        url: u
-    }).then(function(response){
-        $("#nameOfCompany").text(response.companyName);
-        $("#price").text(response.latestPrice);
-    })
+    stocks.push(symbol);
+    $("#stocks").append("<button>");
+    $("#stocks button:last-child").text(symbol);
+})
+
+function checkPrice(){
+    $("#checking").empty();
+    for(var i = 0; i < stocks.length; i++){
+        var symbol = stocks[i];
+        var u = "https://cloud.iexapis.com/stable/stock/"+symbol+"/quote?token=pk_ba9bdda0f20d46cba4e89a3e5a1d5317";
+        $.ajax({
+            method: "GET",
+            url: u
+        }).then(function(response){
+            $("#checking").append("<h2>");
+            $("#checking h2:last-child").text(response.companyName);
+            $("#checking").append("<p>");
+            $("#checking p:last-child").text("Real Time Price: "+response.iexAskPrice +" Latest price: "+ response.latestPrice);
+    
+        }) 
+    }
 }
 
 setInterval(checkPrice, 5000);
