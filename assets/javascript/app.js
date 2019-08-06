f900eeda3adabbb39ef1d0b9f567e6c7b61e5f5e
Artur Markov <artur.markov1860@gmail.com>
2019-08-05 20:27:54 -0700

    login, logout and signup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,25 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyCF0HvWofipxbCwadEyr8aQERIXe9WGr2s",
-    authDomain: "stock-check-e6ce3.firebaseapp.com",
-    databaseURL: "https://stock-check-e6ce3.firebaseio.com",
-    projectId: "stock-check-e6ce3",
-    storageBucket: "",
-    messagingSenderId: "404498187663",
-    appId: "1:404498187663:web:552ae95ccb695dcd"
-  };
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var database = firebase.database();
-
-var stocks = [];
-
-$("#pushStock").on("click", function() {
+$("#pushStock").on("click", function () {
     var symbol = $("input[type=text]").val();
     stocks.push(symbol);
     $("#stocks").append("<button>");
     $("#stocks button:last-child").text(symbol);
 })
 
-function checkPrice(){
+function checkPrice() {
     $("#checking").empty();
-    for(var i = 0; i < stocks.length; i++){
+    for (var i = 0; i < stocks.length; i++) {
         var symbol = stocks[i];
-        var u = "https://cloud.iexapis.com/stable/stock/"+symbol+"/quote?token=pk_ba9bdda0f20d46cba4e89a3e5a1d5317";
+        var u = "https://cloud.iexapis.com/stable/stock/" + symbol + "/quote?token=pk_ba9bdda0f20d46cba4e89a3e5a1d5317";
         $.ajax({
             method: "GET",
             url: u
-        }).then(function(response){
+        }).then(function (response) {
             $("#checking").append("<h2>");
             $("#checking h2:last-child").text(response.companyName);
             $("#checking").append("<p>");
-            $("#checking p:last-child").text("Real Time Price: "+response.iexAskPrice +" Latest price: "+ response.latestPrice);
-    
-        }) 
+            $("#checking p:last-child").text("Real Time Price: " + response.iexAskPrice + " Latest price: " + response.latestPrice);
+
+        })
     }
 }
 
---
+++ assets/javascript/auth.js
@@ -0,0 +1,35 @@
+//signUp
+$("#Sign_Up").on("click", (e) => {
+    event.preventDefault();
+
+    //get user info
+    const email = $("#signUp_email").val();
+    const password = $("#signUp_password").val();
+
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        $("#signUp_form").hide();
+    })
+})
+
+//log out
+$("#log-out").on("click", (e) => {
+    event.preventDefault();
+    auth.signOut().then(() => {
+        console.log("user logged out");
+    })
+
+})
+
+//log In
+$("#Log_In").on("click", (e) => {
+    event.preventDefault();
+
+    //get user info
+    const email = $("#signIn_email").val();
+    const password = $("#signIn_password").val();
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+    })
+})
