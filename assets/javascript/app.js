021cafae172ce7967f7914b98df1666bc1b1c46d
Artur Markov <artur.markov1860@gmail.com>
2019-08-06 17:34:14 -0700

    error with var portfolio solved

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 var portfolio = document.querySelector("#portfolio");
 //display firestoredata in portfolio
 const setupStock = (data) => {
-    if(data.length) {
-        let html ="";
+    if (data.length) {
+        let html = "";
 
         data.forEach(doc => {
             const stock = doc.data();
@@ -14,35 +14,39 @@ const setupStock = (data) => {
             `
             html += displayStock;
         });
-        
-        portfolio.innerHTML = html;
+
+        if (portfolio) {
+            portfolio.innerHTML = html;
+        }
 
     } else {
-        portfolio.innerHTML = "<h1>Please LogIn</h1>";
-}
+        if (portfolio) {
+            portfolio.innerHTML = "<h1>Please LogIn</h1>";
+        }
+    }
 }
 
 //function for checkin price of stock
 $("#pushStock").on("click", function checkPrice() {
     var symbol = $("#symbol").val();
-        $("#checking").empty();
-        // for (var i = 0; i < stocks.length; i++) {
-        var u = "https://cloud.iexapis.com/stable/stock/" + symbol + "/quote?token=pk_ba9bdda0f20d46cba4e89a3e5a1d5317";
-        $.ajax({
-            method: "GET",
-            url: u
-        }).then(function (response) {
-            ;
-            $("#checking").append("<div>");
-            $("#checking div").append("<h2>");
-            $("#checking div h2").text(response.companyName);
-            $("#checking div").append("<p>");
-            $("#checking div p").text("Real Time Price: " + response.iexAskPrice + " Latest price: " + response.latestPrice);
-            var addStock = $("<button>").attr("id", "addStock").text("Add to Portfolio").attr("data-price", response.latestPrice).attr("data-name", response.companyName);
-            $("#checking div").append(addStock);
+    $("#checking").empty();
+    // for (var i = 0; i < stocks.length; i++) {
+    var u = "https://cloud.iexapis.com/stable/stock/" + symbol + "/quote?token=pk_ba9bdda0f20d46cba4e89a3e5a1d5317";
+    $.ajax({
+        method: "GET",
+        url: u
+    }).then(function (response) {
+        ;
+        $("#checking").append("<div>");
+        $("#checking div").append("<h2>");
+        $("#checking div h2").text(response.companyName);
+        $("#checking div").append("<p>");
+        $("#checking div p").text("Real Time Price: " + response.iexAskPrice + " Latest price: " + response.latestPrice);
+        var addStock = $("<button>").attr("id", "addStock").text("Add to Portfolio").attr("data-price", response.latestPrice).attr("data-name", response.companyName);
+        $("#checking div").append(addStock);
 
-        })
-        // }
+    })
+    // }
 })
 
 $(document).on("click", "#addStock", () => {
