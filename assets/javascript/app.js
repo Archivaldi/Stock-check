e97b15140c2fa2089f893b7502443da1c4386323
Artur Markov <artur.markov1860@gmail.com>
2019-08-08 18:55:15 -0700

    dynamic prices

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,21 +5,33 @@ const setupStock = (data) => {
         let html = "";
 
         data.forEach(doc => {
-
             const stock = doc.data();
-            const displayStock = `
+            ////////////////
+            //make app dynamic
+            var symbol = stock.symbol;
+            var u = "https://cloud.iexapis.com/stable/stock/" + symbol + "/quote?token=pk_ba9bdda0f20d46cba4e89a3e5a1d5317";
+            $.ajax({
+                method: "GET",
+                url: u
+            }).then(function (response) {
+                const displayStock = `
                 <div class="displayStock">
-                    <h2>${stock.title}</h2>
-                    <p>${stock.price}</p>
+                    <h2>${response.companyName}</h2>
+                    <p class="symbols">${response.symbol}</p>
+                    <p class="prices">${response.latestPrice}</p>
+                    <p>${response.latestTime}</p>
                 </div>
             `
-            html += displayStock;
+                html += displayStock;
+                //js finding "portfolio" in both html files
+                if (portfolio) {
+                    portfolio.innerHTML = html;
+                }
+            })
+            //////////////////
+
         });
 
-        //js finding "portfolio" in both html files
-        if (portfolio) {
-            portfolio.innerHTML = html;
-        }
 
     } else {
         if (portfolio) {
@@ -32,28 +44,46 @@ const setupStock = (data) => {
 $("#pushStock").on("click", function checkPrice() {
     var symbol = $("#symbol").val();
     $("#checking").empty();
-    // for (var i = 0; i < stocks.length; i++) {
     var u = "https://cloud.iexapis.com/stable/stock/" + symbol + "/quote?token=pk_ba9bdda0f20d46cba4e89a3e5a1d5317";
     $.ajax({
         method: "GET",
         url: u
     }).then(function (response) {
-        ;
+        console.log(response);
         $("#checking").append("<div>");
         $("#checking div").append("<h2>");
         $("#checking div h2").text(response.companyName);
         $("#checking div").append("<p>");
-        $("#checking div p").text("Real Time Price: " + response.iexAskPrice + " Latest price: " + response.latestPrice);
-        var addStock = $("<button>").attr("id", "addStock").text("Add to Portfolio").attr("data-price", response.latestPrice).attr("data-name", response.companyName);
+        $("#checking div p:last-child").text("Real Time Price: " + response.iexAskPrice + " Latest price: " + response.latestPrice);
+        $("#checking div").append("<p>");
+        $("#checking div p:last-child").text(response.latestTime);
+        var addStock = $("<button>").attr("id", "addStock").text("Add to Portfolio").attr("data-symbol", response.symbol).attr("data-name", response.companyName);
         $("#checking div").append(addStock);
-
     })
-    // }
 })
 
 $(document).on("click", "#addStock", () => {
     db.collection("users").doc(userUID).collection("stocks").doc($("#addStock").attr("data-name")).set({
-        title: $("#addStock").attr("data-name"),
-        price: $("#addStock").attr("data-price")
+        symbol: $("#addStock").attr("data-symbol")
     });
 });
+
+
+//automatically checking and changing prices
+function uploadStockInfo() {
+    var stockSymbol = document.querySelectorAll(".symbols");
+    var stockPrice = document.querySelectorAll(".prices");
+    var num = $(portfolio).children().length;
+    for (let i = 0; i < num ; i++) {
+        var u = "https://cloud.iexapis.com/stable/stock/" + stockSymbol[i].innerHTML + "/quote?token=pk_ba9bdda0f20d46cba4e89a3e5a1d5317";
+        $.ajax({
+            method: "GET",
+            url: u
+        }).then(function (response) {
+            debugger;
+            stockPrice[i].innerHTML = response.latestPrice;
+        });
+    };
+};
+
+setInterval(uploadStockInfo, 10000);
